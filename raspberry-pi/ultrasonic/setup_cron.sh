2e50ee15f3580161f3d33de7c4889a9a6d3f7710
jitesh <jitesh@helloworld.co.in>
2021-07-07 09:54:54 +0100

    ...

--- raspberry-pi/ultrasonic/setup_cron.sh
+++ raspberry-pi/ultrasonic/setup_cron.sh
@@ -2,9 +2,15 @@
 
 FILE_NAME="sensor.py"
 
-crontab -l > mycron
-echo "* * * * * sudo python $PWD/$FILE_NAME &" >> mycron
-crontab mycron
-rm mycron
+crontab -u $USER -l| grep $PWD/$FILE_NAME > /dev/null
+if [ $? -eq 0 ]
+then
+	echo "$FILE_NAME is already running"
+else
+	crontab -l > mycron
+	echo "* * * * * sudo python $PWD/$FILE_NAME &" >> mycron
+	crontab mycron
+	rm mycron
+	echo "cron task added. Now $PWD/$FILE_NAME file will run every minute"
+fi
 
-echo "cron task added. Now $PWD/$FILE_NAME file will run every minute"
