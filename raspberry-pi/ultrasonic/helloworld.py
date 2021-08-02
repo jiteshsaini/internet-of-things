509f2e8b089a1caa40b206b0e5771ed8a25846ab
Jitesh <46092054+jiteshsaini@users.noreply.github.com>
2021-08-02 13:54:32 +0100

    Update helloworld.py

--- raspberry-pi/ultrasonic/helloworld.py
+++ raspberry-pi/ultrasonic/helloworld.py
@@ -1,8 +1,9 @@
-import os, json
-
-auth_token='xxxx'
+auth_token='ed5d1166c007dd2f068cd31636de6912'
 
 sensorType_no="ultrasonic_1"
+
+#======DO NOT MODIFY THE CODE BELOW =======================#
+import os, json
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 curr_dir=os.path.abspath(os.getcwd())
@@ -12,22 +13,23 @@ def upload_data(reading):
 	url_remote="https://helloworld.co.in/iot/data.php?"+parameters
 	cmd="curl -s " + url_remote
 	#print (cmd)
-	result=os.popen(cmd).read()
-	xx=json.loads(result)
-	
-	if(xx["settings_flag"]=="1"):
-		f = open(curr_dir+"/util/settings.json", "wb")
-		f.write(result)
-		f.close()
-		print(json.dumps(xx,indent=1))
-		
-	else:
-		print (xx["server_msg"])
-		
-		if(xx["state"]=="error"):
-			f = open(curr_dir+"/util/error_log.txt", "a")
-			f.write(xx["server_time"] + ":" + xx["server_msg"] +"\n")
-			f.close()
+	try:
+	  result=os.popen(cmd).read()
+	  xx=json.loads(result)
+	  if(xx["settings_flag"]=="1"):
+		  f = open(curr_dir+"/util/settings.json", "wb")
+		  f.write(result)
+		  f.close()
+		  print(json.dumps(xx,indent=1))
+	  else:
+		 print (xx["server_msg"])
+		 if(xx["state"]=="error"):
+			 f = open(curr_dir+"/util/error_log.txt", "a")
+			 f.write(xx["server_time"] + ":" + xx["server_msg"] +"\n")
+			 f.close()
+	except:
+		print("..")
+		pass
 		
 
 def read_settings():
